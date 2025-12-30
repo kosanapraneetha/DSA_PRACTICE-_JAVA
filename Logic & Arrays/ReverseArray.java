027785253292b848f04b2528653598bbc338d68f
Praneetha Kosana <kosanapraneetha@gmail.com>
2025-12-30 23:36:48 +0530

    Create ReverseArray.java

---
+++ Logic & Arrays/ReverseArray.java
@@ -0,0 +1,19 @@
+
+import java.util.Arrays;
+public class ReverseArray{
+    public static void reverseArray(int[] arr){
+        int left=0,right=arr.length-1;  //using 2-pointers
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
