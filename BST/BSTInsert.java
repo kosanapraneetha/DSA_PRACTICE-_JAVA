63ebb8c1d93c263dd1cae01c5b0d0d020a2cbdb3
Praneetha Kosana <kosanapraneetha@gmail.com>
2026-01-06 23:05:01 +0530

    Create BSTInsert.java

---
+++ BST/BSTInsert.java
@@ -0,0 +1,42 @@
+
+public class BST{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
+    Node root;
+    public void insert(int data){
+        root=insert(root,data);
+    }
+    private Node insert(Node node,int data){
+        if(node==null) return new Node(data);
+        if(data<node.data) {
+        node.left=insert(node.left,data);
+        }
+        else if(data>node.data) {
+            node.right=insert(node.right,data);
+        }
+        return node;
+    }
+    public static void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+    public static void main(String[] args){
+        BST obj=new BST();
+        obj.insert(8);
+        obj.insert(10);
+        obj.insert(13);
+        obj.insert(15);
+        obj.insert(23);
+       inorder(obj.root);
+    }
+}
